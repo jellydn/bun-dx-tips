dd08d9761bc5629cce66df0598631aec14639f84
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-08 17:04:38 +0800

    feat: add hostname to support running from docker

--- server.ts
+++ server.ts
@@ -3,14 +3,15 @@ import express from "express";
 
 const app = express();
 const port = Bun.env.PORT ?? 8080;
+const hostname = Bun.env.HOSTNAME ?? "localhost";
 
 app.get("/", (_req: express.Request, res: express.Response) => {
   logger.info("Hello World!");
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   logger.info(
-    `Server started at ${Bun.env.SERVER_URL ?? `http://localhost:${port}`}`,
+    `Server started at ${Bun.env.SERVER_URL ?? `http://${hostname}:${port}`}`,
   );
 });
--- types.d.ts
+++ types.d.ts
@@ -2,5 +2,6 @@ declare module "bun" {
   interface Env {
     PORT: number;
     SERVER_URL: string;
+    HOSTNAME: string;
   }
 }
